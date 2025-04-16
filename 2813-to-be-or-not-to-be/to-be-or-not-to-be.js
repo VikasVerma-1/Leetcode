fbae1c3ca8774cb9e63b88f6efbeae8ca670da11
Vikas Verma <110451386+VikasVerma-1@users.noreply.github.com>
2025-04-16 12:32:33 +0530

    Time: 47 ms (29.91%) | Memory: 54.9 MB (5.85%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(expected) {
+            if (val === expected) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: function(unexpected) {
+            if (val !== unexpected) return true;
+            throw new Error("Equal");
+        }
+    };
+};
