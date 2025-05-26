4129bd05cd75a9c103cce40dd8c64e6c2e8d2088
Vikas Verma <110451386+VikasVerma-1@users.noreply.github.com>
2025-05-26 18:36:04 +0530

    Time: 41 ms (61.84%) | Memory: 54.2 MB (14.28%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,10 @@
+
+var filter = function(arr, fn) {
+    result=[];
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i], i)){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
